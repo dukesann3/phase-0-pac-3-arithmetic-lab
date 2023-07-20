7c5db6b899bfe84c25b9839d48fc7cb9bf9f3790
Duke <dukemorise33@gmail.com>
2023-07-19 22:31:49 -0700

    passed tests for phase-0-pac-3-arithmetic-lab

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+    return a+b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+function increment(a){
+    a++;
+    return a;
+}
+
+function decrement(a){
+    a--;
+    return a;
+}
+
+function makeInt(n){
+    n = parseInt(n,10);
+    return n;
+}
+
+function preserveDecimal(n){
+    n = parseFloat(n);
+    return n;
+}
+
